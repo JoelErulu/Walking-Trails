b5f806915c9ff7ce1dd67c893209efa8a383a3e0
Ginette <awilsonbahun@ggc.edu>
2024-09-29 01:02:18 -0400

    updated input.js

--- TrailApp/client/src/components/Authorization/input.js
+++ TrailApp/client/src/components/Authorization/input.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
-
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import { Form, Row, Col, Button, InputGroup } from 'react-bootstrap';
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Using  icons for visibility toggle
 
 const Input = ({ name, handleChange, label, half, autoFocus, type, handleShowPassword }) => (
-    <Grid item xs={12} sm={half ? 6 : 12}>
-        <TextField 
-            name={name}
-            onChange={handleChange}
-            variant="outlined"
-            required
-            fullWidth
-            label={label}
-            autoFocus={autoFocus}
-            type={type}
-            InputProps={name === 'password' ? {
-                endAdornment: (
-                    <InputAdornment position="end">
-                        <IconButton onClick={handleShowPassword}>
-                            {type === "password" ? <Visibility /> : <VisibilityOff />}
-                        </IconButton>
-                    </InputAdornment>
-                ),
-            } : null}
-        />
-    </Grid>
+    <Form.Group as={Row} className="mb-3">
+        <Form.Label column xs={12} sm={half ? 6 : 12}>
+            {label}
+        </Form.Label>
+        <Col xs={12} sm={half ? 6 : 12}>
+            <InputGroup>
+                <Form.Control
+                    name={name}
+                    onChange={handleChange}
+                    required
+                    autoFocus={autoFocus}
+                    type={type}
+                />
+                {name === 'password' && (
+                    <InputGroup.Append>
+                        <Button variant="outline-secondary" onClick={handleShowPassword}>
+                            {type === "password" ? <FaEye /> : <FaEyeSlash />}
+                        </Button>
+                    </InputGroup.Append>
+                )}
+            </InputGroup>
+        </Col>
+    </Form.Group>
 );
 
 export default Input;

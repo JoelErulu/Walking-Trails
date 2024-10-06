b2922c8419c1c9e919cd3452550a14137d3b4bdb
J Haines <jhaines2@ggc.edu>
2024-10-06 15:39:31 -0400

    AdminManager

--- TrailApp/client/src/pages/AdminManager.js
+++ TrailApp/client/src/pages/AdminManager.js
@@ -1,6 +1,6 @@
 // Import React components
-import React from 'react';
-//import React, { useEffect } from 'react';
+//import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUsers, updateUserRole } from '../actions/users';
 
@@ -9,9 +9,76 @@ import { getUsers, updateUserRole } from '../actions/users';
 
 // Import global stylesheet
 import '../interfaceSettings.css';
+import { useState } from "react";
+import { fetchUsers } from '../api/index.js';
 
 // README: 
 const AdminManager = () => {
+
+    const dispatch = useDispatch();
+    const users = useSelector((state) => state.users.users);
+
+    useEffect(() => {
+        dispatch(getUsers());
+    }, [dispatch]);
+
+    const handleCheckboxChange = (userId, isChecked) => {
+        dispatch(updateUserRole(userId, {role: isChecked ? 'admin' : 'user'}));
+        console.log(users);
+        //click on checkbox
+        //update db
+        //refresh page
+        //checkbox? ==> dispatch 
+        };
+
+    const Rolecheckbox = ({ checked, onChange }) => {
+        return (
+            /*
+            <input class="form-check-input" type="checkbox" value="Admin" id="RoleCheckbox"
+                          color="secondary"
+              checked={checked}
+              onChange={(e) => onChange(e.target.checked)}
+            >*/
+            <select class="form-control" type="checkbox" value="Admin" id="RoleCheckbox"
+            color="secondary"
+              checked={checked}
+              onChange={(e) => onChange(e.target.checked)}>
+                
+            <label className="input-label" for="selectRole">Assign Admin Privileges</label>
+                <option>User Role</option>
+                <option>Admin Role</option>
+            </select>
+            
+        );
+    }
+
+    const Checkbox = ({ label }) => {
+        const [isChecked, setIsChecked ] = useState(false); 
+        return (
+            <div className='checkbox-wrapper'>
+                <p>{getUsers()}</p>
+                <label>
+                    <input type="checkbox" checked={isChecked}
+                    onChange={() => setIsChecked((prev) => !prev)}/>
+                    <span>{label}</span>
+                </label>
+                <p>{isChecked ? "Selected" : "Unchecked"}</p>
+            </div>
+        );
+    } 
+
+    
+
+    const Testbox = () => {
+        return (
+            <div>
+                <p>Pilchmore</p>
+                <Checkbox label="User" />
+                <Checkbox label="Admin"/>
+            </div>
+        );
+    }
+
 
     return (
      <section id="AdminManager" className="container mt-5">
@@ -33,27 +100,26 @@ const AdminManager = () => {
                 </div>
 
                 <p className="lead mt-4 input-label">Assign Admin Privileges</p>
-                
-                {/*
-                <div class="form-group">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                <label class="form-check-label" for="flexCheckDefault">
-                <p className="lead">User Role</p>
-                </label>
-                    
+                <div class ="form-group">
+                <p>The</p>
+                {<Testbox />}
+                {users && users.map((user) => (
+                <Rolecheckbox
+                    checked={user.role === 'admin'}
+                    onChange={(isChecked) =>
+                    handleCheckboxChange(user._id, isChecked)
+                    }
+                />
+                ))}
                 </div>
-                <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                <label class="form-check-label" for="flexCheckDefault">
-                <p className="lead" >Admin Role</p>
-                </label>
-                </div>*/}
 
+                {/*
                 <select class="form-control" id="exampleFormControlSelect1">
                 <label className="input-label" for="exampleInputEmail1">Assign Admin Privileges</label>
                     <option>User Role</option>
                     <option>Admin Role</option>
                 </select>
+                */}
                 <br></br>
                 <br></br>
                 <button type="submit" class="btn btn-primary btn-responsive btn-block mt-4">Submit</button>

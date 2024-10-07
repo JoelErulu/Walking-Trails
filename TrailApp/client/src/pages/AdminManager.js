5ccd82586ba49cba93fd8ce102199176063fe60d
J Haines <jhaines2@ggc.edu>
2024-10-07 15:35:29 -0400

    Updated AdminManager page

--- TrailApp/client/src/pages/AdminManager.js
+++ TrailApp/client/src/pages/AdminManager.js
@@ -3,6 +3,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUsers, updateUserRole } from '../actions/users';
+import { fetchUserById } from '../api/index.js';
 
 // Import components
 //import { getUsers, updateUserRole } from '../../../actions/users';
@@ -11,6 +12,7 @@ import { getUsers, updateUserRole } from '../actions/users';
 import '../interfaceSettings.css';
 import { useState } from "react";
 import { fetchUsers } from '../api/index.js';
+import {useRef} from 'react';
 
 // README: 
 const AdminManager = () => {
@@ -55,77 +57,102 @@ const AdminManager = () => {
     const Checkbox = ({ label }) => {
         const [isChecked, setIsChecked ] = useState(false); 
         return (
-            <div className='checkbox-wrapper'>
-                <p>{getUsers()}</p>
-                <label>
-                    <input type="checkbox" checked={isChecked}
-                    onChange={() => setIsChecked((prev) => !prev)}/>
-                    <span>{label}</span>
-                </label>
-                <p>{isChecked ? "Selected" : "Unchecked"}</p>
-            </div>
+                <input className="p-4" type="checkbox" checked={isChecked}
+                onChange={() => setIsChecked((prev) => !prev)}/>
+
         );
-    } 
+    }
 
     
+    const emailRef = useRef(null);
+    const [message, setMessage] = useState('');
 
-    const Testbox = () => {
-        return (
-            <div>
-                <p>Pilchmore</p>
-                <Checkbox label="User" />
-                <Checkbox label="Admin"/>
-            </div>
+    const handleSubmit = event => {
+        console.log('handleSubmit ran');
+        event.preventDefault();
+    
+        console.log('email 👉️', emailRef.current.value);
+    
+        setMessage(
+          `${emailRef.current.value}`,
         );
+    
+        event.target.reset();
+      };
+    
+
+    const Emailselector = () => {    
+
+        return (
+          <div>
+            <form onSubmit={handleSubmit}>
+            <div class="form-group">
+            <label className="input-label" for="exampleInputEmail1">Email Address</label>
+              <input
+                class="form-control"
+                ref={emailRef}
+                id="email_select"
+                name="email_select"
+                type="text"
+                placeholder="Email"
+              />
+              <small id="emailHelp" class="form-text text-muted">Must enter a valid user email registered in our database.</small>
+
+              <br />
+              </div>      
+              <button type="submit" class="btn btn-primary btn-responsive btn-block mt-4">Select Profile</button>
+      
+              
+            </form>
+          </div>
+        );
+    };
+
+    const Displayprofileinformation = () => {
+        
+        return(
+            <div>
+                <h2>Profile Information</h2>
+                <p className="lead">Current selected profile: <b>{message}</b>
+                </p>
+                <h3>User Roles:</h3>
+                <div className="container">
+                    <p className='lead'>{<Checkbox label="User" />} User</p>
+                    <p className='lead'>{<Checkbox label="Admin"/>} Admin</p>
+                </div>
+                {/*
+                <select class="form-control" id="exampleFormControlSelect1">
+                <label className="input-label" for="exampleInputEmail1">Assign Admin Privileges</label>
+                    <option>User Role</option>
+                    <option>Admin Role</option>
+                </select>*/}
+                <button type="submit" class="btn btn-primary btn-responsive btn-block mt-4">Update Profile</button>
+
+            </div>
+        )
+    }
+
+    const Updateprofile = () => {
+        //get user by email
+
+        //update user role by id
     }
 
 
     return (
-     <section id="AdminManager" className="container mt-5">
         <div className="container-xl">
         <div className="container">
-        <div className="row">
+        <div className="row align-items-center">
         <div className="col-lg-6 col-md-12 p-3">
             <h2>Admin Management Portal</h2>
             <p className="lead my-4">Here you will be able to change a registered user's role! Just fill out the form below.</p>
             {/* Insert input form that takes in user email & searches for them in database to update their roleType */}
             {/* Reuse code from oldDirectory/admin/AdminPrivilege*/}
 
-           
-                <form>
-                <div class="form-group">
-                    <label className="input-label" for="exampleInputEmail1">Email Address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-                    <small id="emailHelp" class="form-text text-muted">Must enter a valid user email registered in our database.</small>
-                </div>
-
-                <p className="lead mt-4 input-label">Assign Admin Privileges</p>
-                <div class ="form-group">
-                <p>The</p>
-                {<Testbox />}
-                {users && users.map((user) => (
-                <Rolecheckbox
-                    checked={user.role === 'admin'}
-                    onChange={(isChecked) =>
-                    handleCheckboxChange(user._id, isChecked)
-                    }
-                />
-                ))}
-                </div>
-
-                {/*
-                <select class="form-control" id="exampleFormControlSelect1">
-                <label className="input-label" for="exampleInputEmail1">Assign Admin Privileges</label>
-                    <option>User Role</option>
-                    <option>Admin Role</option>
-                </select>
-                */}
-                <br></br>
-                <br></br>
-                <button type="submit" class="btn btn-primary btn-responsive btn-block mt-4">Submit</button>
-            
-                <hr />
-                </form>
+            {<Emailselector />}
+            <hr />
+            {<Displayprofileinformation />}
+            <hr />
                 <p className="lead my-4">Here you will be able to upload, delete, and update videos! Just fill out the form below.</p>
                 {/* Insert input form that takes in video URL &/or file and updates the database */}
                 <form>
@@ -139,7 +166,6 @@ const AdminManager = () => {
             </div>
 
         </div>
-        </section>
     )
 }
 

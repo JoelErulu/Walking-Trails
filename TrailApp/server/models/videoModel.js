03ff331cdb328d2dff0539571fba7f880c1a881c
Ginette <awilsonbahun@ggc.edu>
2024-11-14 16:06:12 -0500

    updated app.js

--- TrailApp/server/models/videoModel.js
+++ TrailApp/server/models/videoModel.js
@@ -1,5 +1,10 @@
 import mongoose from 'mongoose';
 
+const reactionSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    reaction: { type: String, enum: ['like', 'dislike'], required: true },
+});
+
 const videoSchema = new mongoose.Schema({
     title: { type: String, required: true },
     url: { type: String, required: true },
@@ -7,7 +12,8 @@ const videoSchema = new mongoose.Schema({
     subCategory: { type: String, required: true },
     totLikes: { type: Number, default: 0 },
     totDislikes: { type: Number, default: 0 },
-    views: { type: Number, default: 0 }, 
+    views: { type: Number, default: 0 },
+    reactions: [reactionSchema], // New field to store user reactions
 }, { timestamps: true });
 
 const Videos = mongoose.model('Videos', videoSchema);

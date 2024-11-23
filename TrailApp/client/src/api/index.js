cf3efe6dffe671e59132dd522bbeee1caba86e05
Joel <135941675+JoelErulu@users.noreply.github.com>
2024-11-22 19:58:06 -0500

    Update index.js

--- TrailApp/client/src/api/index.js
+++ TrailApp/client/src/api/index.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://walking-trails-2.onrender.com' });
 
 
 export const signUp = (formData) => API.post('/api/user/signup', formData);
